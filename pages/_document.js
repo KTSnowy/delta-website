931fcc4b93f4073c1f483029d3554ef0166160de
Keith Taylor <KTSnowy@outlook.com>
2021-10-07 19:26:12 -0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -14,12 +14,12 @@ class DeltaDocument extends Document {
         <meta property="og:title" content="Delta Informatica" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://delta-website-eight.vercel.app/" />
-        <meta property="og:image" content="banner.png" />
+        <meta property="og:image" content="https://delta-website-eight.vercel.app/banner.png" />
         {'Twitter Tags'}
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content="Delta Informática" />
         <meta name="twitter:description" content="Com mais de 20 anos é a mais antiga recarga de cartuchos de Madureira" />
-        <meta name="twitter:image" content="banner.png" />
+        <meta name="twitter:image" content="https://delta-website-eight.vercel.app/banner.png" />
         <meta name="theme-color" content="#202020" />
         </Head>
         <body>
